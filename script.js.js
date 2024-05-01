3a6b34ed67a536e25be0eb4037f678c4bc089bb1
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-05-01 23:46:21 +0100

    Made the gallery app look better and work smoother on different devices.

---
+++ script.js.js
@@ -0,0 +1,18 @@
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter"){
+        let searcValue = search.value,
+            value = searcValue.toLowerCase();
+            images.forEach(image =>{
+                if(value === image.dataset.name){ //matching value with getting attribute of images
+                    return image.style. display = "block";
+                }
+                image.style. display = "none";
+         });
+    }
+});
+search.addEventListener("keyup", () =>{
+    if(search.value != "") return;
+    images.forEach(image =>{
+        image.style.display = "block";
+    })
+})
